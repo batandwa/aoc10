dbcb1661ce895f5a4a539384d9b27ad8905c5ba6
Batandwa Colani <i@batandwa.me>
2018-02-27 21:33:12 +0200

    Removed use of CLImate.

--- main.php
+++ main.php
@@ -1,28 +1,10 @@
 <?php
 require_once('vendor/autoload.php');
 
-$climate = new League\CLImate\CLImate();
+$length = 256;
+$arguments = $argv;
+unset($arguments[0]);
 
-$climate->arguments->add([
-    'length' => [
-        'prefix' => 'l',
-        'longPrefix' => 'length',
-        'description' => 'The length of the hash.',
-        'defaultValue' => '256'
-    ],
-    'inputlist' => [
-        'description' => 'Comma delimited sequence of numbers to use as input to the algorithm.',
-    ],
-]);
-
-$climate->arguments->parse();
-// TODO: Test for non-integer values.
-$length = (int)$climate->arguments->get('length');
-$inputList = $climate->arguments->get('inputlist');
-
-$game = new AOC10\Game($length, $inputList);
+$game = new AOC10\Game($length, implode('', $arguments));
 $hash = $game->process();
-
-$climate->br()->green()->out(implode('', $hash));
-
-
+print implode('', $hash) . "\n";
