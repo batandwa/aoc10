1194dbd28e1bb74b8d3f0977999d0bc4a666ba09
Batandwa Colani <i@batandwa.me>
2018-02-27 20:54:42 +0200

    Updated tests for part 2 hashing method.

--- tests/GameTest.php
+++ tests/GameTest.php
@@ -2,25 +2,36 @@
 use PHPUnit\Framework\TestCase;
 
 final class GameTest extends TestCase {
-    public function testSimpleReverse(): void {
-        $game = new AOC10\Game(5);
-
-        $game->processLength(3);
-        $this->assertEquals($game->getList(), [2, 1, 0, 3, 4]);
-
-        $game->processLength(4);
-        $this->assertEquals($game->getList(), [4, 3, 0, 1, 2]);
-
-        $game->processLength(1);
-        $this->assertEquals($game->getList(), [4, 3, 0, 1, 2]);
-
-        $game->processLength(5);
+    /**
+     * @Deprecated
+     */
+    public function xtestSimpleReverse(): void {
+        $game = new AOC10\Game(5, "3415");
+        $game->process();
         $this->assertEquals($game->getList(), [3, 4, 2, 1, 0]);
     }
 
     public function testInvalidLength(): void {
         $this->expectException(InvalidArgumentException::class);
-        $game = new AOC10\Game(5);
-        $game->processLength(10);
+        $game = new AOC10\Game(5, "9");
+        $game->process();
+    }
+
+    public function testSequenceToHash(): void {
+        $game = new AOC10\Game(256, "AoC 2017");
+        $hash = $game->process();
+        $this->assertEquals('33efeb34ea91902bb2f59c9920caa6cd', implode('', $hash));
+
+        $game = new AOC10\Game(256, '');
+        $hash = $game->process();
+        $this->assertEquals('a2582a3a0e66e6e86e3812dcb672a272', implode('', $hash));
+
+        $game = new AOC10\Game(256, '1,2,3');
+        $hash = $game->process();
+        $this->assertEquals('3efbe78a8d82f29979031a4aa0b16a9d', implode('', $hash));
+
+        $game = new AOC10\Game(256, '1,2,4');
+        $hash = $game->process();
+        $this->assertEquals('63960835bcdc130f0b66d7ff4f6a5a8e', implode('', $hash));
     }
 }
